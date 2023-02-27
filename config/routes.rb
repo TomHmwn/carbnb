f64dc7cf1f630467307225bb4fae5ac3bf4acaf2
max reid <maxwilliamreid@gmail.com>
2023-02-27 15:30:53 +1100

    halfway done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+  resources :cars, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
