46d51a24fb02ee96502f81d630130e101ce34754
Thomas Himawan <thimawan@student.unimelb.edu.au>
2023-02-27 22:37:18 +1100

    added car listing and each listing of car

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,32 @@
 class CarsController < ApplicationController
+  def index
+    @cars = Car.all
+  end
+
+  def new
+    @car = Car.new
+  end
+
+  def show
+    @car = Car.find(params[:id])
+  end
+
+  def create
+    @car = Car.new(car_params)
+    @car.user = current_user
+    if @car.save
+      redirect_to car_path(@car)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_car
+    @car = Car.find(params[:id])
+  end
+  def car_params
+    params.require(:car).permit(:model, :brand, :color, :price_per_day, :address, :image)
+  end
 end
