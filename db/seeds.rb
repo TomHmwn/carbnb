f55dddbc86071ccbc2e3e14ad34ab6f2bca6d00c
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-02-27 16:49:03 +1100

    populated database using faker and updated cars model to include image

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Clearing old data
+puts "Clearing out old data"
+Car.destroy_all
+User.destroy_all
+# Booking.destroy_all
+
+# Creating users
+puts "Creating users"
+10.times {
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  email_address = "#{first_name}#{last_name}@gmail.com"
+  new_user = User.new(first_name:, last_name:, email_address:)
+  new_user.save!
+}
+puts "Finished creating users"
+
+# Creating cars
+puts "Creating cars"
+10.times {
+  # puts "#{User.all.sample}"
+  user_id = User.all.sample.id
+  color = Faker::Vehicle.color
+  fake_car = Faker::Vehicle.make_and_model.split
+  brand = fake_car.first
+  model = fake_car.last
+  address = Faker::Address.street_address
+  image = "http://loremflickr.com/280/280/#{brand}#{model}"
+  price_per_day = rand(50..100)
+  new_car = Car.new(color:, model:, brand:, price_per_day:, user_id:, address:, image:)
+  new_car.save!
+}
+puts "Finished creating cars"
