8481243bca34c03f1290f0e5b5cdf1aa033b8d91
max reid <maxwilliamreid@gmail.com>
2023-02-27 15:58:30 +1100

    bulk of html done, need to update links once routes have been created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  resources :cars, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :cars
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
