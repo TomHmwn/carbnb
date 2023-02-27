89af8cb1ddb7c7f83ff16ea0bd01a7a825eab828
max reid <maxwilliamreid@gmail.com>
2023-02-27 17:18:09 +1100

    fixed user email problem

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_042512) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_02_27_061153) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,7 +42,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_042512) do
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "email_address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
