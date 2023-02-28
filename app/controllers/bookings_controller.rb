17bf6d21ad3fc63a9045d0a2e090eb781e243b58
Andy Yin <yin.andy32@gmail.com>
2023-02-28 17:31:00 +1100

    Small changes made to file

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -62,9 +62,11 @@ class BookingsController < ApplicationController
   end
 
   private
+
   def set_booking
     @booking = Booking.find(params[:id])
   end
+
   def booking_params
     params.require(:booking).permit(:start_date, :end_date)
   end
