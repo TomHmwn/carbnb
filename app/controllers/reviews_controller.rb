9b2818edf0ec67ca90e49adcc9ea14e04b94f06d
max reid <maxwilliamreid@gmail.com>
2023-03-01 12:26:57 +1100

    required validations to review rating and content, beefed up reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,27 @@
 class ReviewsController < ApplicationController
+  before_action :set_car, only: [:new, :create]
   def new
-    @car = Car.find(params[:car_id])
     @review = Review.new
   end
 
   def create
     @review = Review.new(review_params)
     @review.user = current_user
-    @review.car = Car.find(params[:car_id])
+    @review.car = @car
     if @review.save
-      redirect_to car_path(@review.car)
+      redirect_to car_path(@car)
     else
       render :new, status: :unprocessable_entity, notice: "Review not saved"
     end
+  end
+
+  private
+
+  def set_car
+    @car = Car.find(params[:car_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
