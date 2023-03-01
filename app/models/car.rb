222ab2acda3cb3434bb1a56f9619c2494da631a0
Andy Yin <yin.andy32@gmail.com>
2023-03-01 14:49:43 +1100

    Added validations for new cars to require all details

--- app/models/car.rb
+++ app/models/car.rb
@@ -4,6 +4,13 @@ class Car < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many_attached :photos
 
+  validates :brand, presence: true
+  validates :model, presence: true
+  validates :color, presence: true
+  validates :price_per_day, presence: true, numericality: { :greater_than_or_equal_to => 0 }
+  validates :address, presence: true
+  validates :photos, presence: true
+
   attribute :remove_file, :boolean, default: false
   after_save :purge_file, if: :remove_file
 end
